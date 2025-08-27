bacf9cc9ee5629cabe4902540e20dd28cf336141
Kapelo256 <105801565+Kapelo256@users.noreply.github.com>
2025-08-27 13:58:54 +0100

    first commit

---
+++ src/lib/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import * as schema from './schema/index';
+
+import * as dotenv from 'dotenv';
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("❌ DATABASE_URL is not set in .env.local");
+}
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
